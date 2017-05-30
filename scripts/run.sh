45a5dc5b881b85f70eb0c02f8b1c729d8b2af8bd
divramod <divramod@gmail.de>
2017-05-30 11:56:29 +0200

    remove run.sh

--- scripts/run.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -p 4000:4000 -v ~/blog:/blog -it divramod/hexo hexo server
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
 cd ~/blog
 git pull
 sudo docker run -v ~/blog:/blog -it divramod/hexo hexo generate
+sudo docker run -p 4000:4000 -v ~/blog:/blog -it divramod/hexo hexo server
